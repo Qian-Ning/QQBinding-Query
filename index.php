4ac4a1a8647d5b1161fea979f57ac62bde3b3d4e
QianNing <qian-ning@foxmail.com>
2025-02-13 15:03:46 +0800

    Q-query

---
+++ index.php
@@ -0,0 +1,231 @@
+</SCRIPT>
+<html lang="zh-cn"><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>免费Q绑在线查询</title>
+<meta name="keywords" content="Q绑在线查询,16亿q绑查询,Q绑8e数据库,16亿qq绑定数据查询,查q绑,查地址,免费Q绑,Q绑QQ查询,q绑在线查询,在线查q绑,在线查绑,q绑查询,q绑在线网页查询,q绑e数据库,q绑在线查询,q绑在线查询官网,免费查询q绑,手机号,微博,英雄联盟,lol,qq密码,社工库,查q绑手机号网站,q绑查询手机号免费网站,2024q绑在线查询,qq查绑接口api,亿q绑查询,社工库查询,查地址,查q绑机器人,免费q绑接口,q绑qq查询接口">
+<meta name="description" content="在线Q绑查询手机号官网，输入QQ号即可查询，Q绑信息查询，就是通过QQ号可以查询到绑定的手机号，但不是所有的QQ号都能查询得到。Q绑数据只是社工数据库中的冰山一角，其他还有很多意想不到的信息多如牛毛，在这个大数据时代，注重个人隐私信息保护，已经不是一件小事 ">
+<link href="/static/css/bootstrap.min.css" rel="stylesheet">
+<script src="/static/js/jquery.min.js"></script>
+<script src="/static/js/layer.js"></script>
+<link rel="stylesheet" href="/static/css/layer.css" id="layuicss-layer">
+<![endif]-->
+    <script>
+    document.oncontextmenu=new Function("event.returnValue=false;"); //禁止右键功能
+    document.onkeydown=MM_KeyPress;
+    function  MM_KeyPress(num){
+        //防止系统退格键
+        var keycODe = event.keyCODe;
+        if(keycode ==8)//屏蔽退格健
+        {
+            event.keyCode = 0;
+            return;
+        }
+        if(keycode >=122 && keycode <=123)//屏蔽f12功能键
+        {
+            event.keyCode = 0 ;
+            event.returnValue=false;
+            return;
+        }
+        
+    }
+    
+    function gg(){
+    layer.alert('您好！欢迎来到千凝免费Q绑在线查询！本站由千凝网提供技术支持', {
+  icon: 1,
+  title: '公告',
+  skin: 'layer-ext-moon'
+})
+}
+function click(e) {
+if (document.all) {
+if (event.button==2||event.button==3) { alert("欢迎光临寒舍，有什么需要帮忙的话，请与站长联系！谢谢您的合作！！！");
+oncontextmenu='return false';
+}
+}
+if (document.layers) {
+if (e.which == 3) {
+oncontextmenu='return false';
+}
+}
+}
+if (document.layers) {
+document.captureEvents(Event.MOUSEDOWN);
+}
+document.onmousedown=click;
+document.oncontextmenu = new Function("return false;")
+document.onkeydown =document.onkeyup = document.onkeypress=function(){
+if(window.event.keyCode == 123) {
+window.event.returnValue=false;
+return(false);
+}
+}
+</script>
+<body background="/static/images/bj.jpg">
+<div class="container">
+<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 center-block" style="float: none;">
+<br>
+<div class="panel panel-primary">
+<div class="panel-heading" style="background: linear-gradient(to right,#b221ff,#14b7ff,#8ae68a);">
+<h3 class="panel-title">Q绑在线查询</h3>
+</div>
+<div class="panel-body" style="text-align: center;">
+<div class="list-group">
+<div class="list-group-item list-group-item-warning" style="font-weight: bold;">
+<span>输入QQ号码即可查询密保手机</span>
+</div>
+<div class="list-group-item list-group-item-info" style="font-weight: bold;">
+<input class="form-control" id="input" placeholder="请输入QQ">
+</div>
+			<marquee scrollamount="5" direction="left" align="Middle" style="font-weight: bold;line-height: 15px;font-size: 15px;color: #FF0000;">请输入需要查询的QQ号 然后点击查找！请勿用于非法用途 只供查询自己QQ是否泄露！​</marquee>
+<div class="list-group-item">
+<a onclick="qbang('cha')" class="btn btn-block btn-primary" style="background: linear-gradient(to right,#b221ff,#14b7ff);">查找</a>
+
+<a class="btn btn-block btn-primary" style="background: linear-gradient(to right,#5A5A5A,#5A5A5A);" href="/fc.php">手机查询</a>
+<a class="btn btn-block btn-primary" style="background: linear-gradient(to right,#48C9B0,#28B463);" href="https://vlink.cc/mecc">官方导航</a>
+</div>
+<div class="pc_acmsd"><center><a href="https://vlink.cc/mecc" target="_blank"><img src="https://pic.imgdb.cn/item/6643cd240ea9cb1403b617f0.png" style="width:100%;Height:60px;border-radius: 5px;padding: 0px;margin-top: 0px;margin-bottom: 1px;" alt="广告合作联系站长QQ：5218641"></a></center></div>
+
+                    <table class="table table-bordered">
+                        <tbody>
+                            
+                                                           </tr>
+                           
+                                <td>
+                                    <a href="https://vcdn.vlink.cc/qrcode/mecc_VB9KHgWveW.jpg" class="btn btn-block btn-primary" target="_blank">人工查询</a>
+                                
+                                </td>
+                                <td>
+                                    <a href="https://salzwa1ojew.feishu.cn/wiki/VQ48wUTKfisTtYku0wjc0KVunIe" class="btn btn-block btn-primary" target="_blank">科学上网</a>
+                                </td>
+                            </tr>
+                            <tr height="50">
+                                <td>
+                                    <a href="https://haokawx.lot-ml.com/Product/Index/605614" class="btn btn-block btn-success" target="_blank">正规流量卡</a>
+                                </td>
+                                <td>
+                                    <a href="https://pan.quark.cn/s/5757c5b7c872#/list/share" class="btn btn-block btn-success" target="_blank">短信测压宝</a>
+                          </td>
+                            </tr>
+                            <tr height="50">
+                            
+                               
+                            </tr>
+                            <tr height="50">
+                                <td>
+                                    <a href="https://m.baidu.com/s?word=ccou.cc" class="btn btn-block btn-danger" target="_blank">点我一下</a>
+                                </td>
+                                <td>
+                                    <a href="https://www.baidu.com/s?wd=ccou.cc" class="btn btn-block btn-danger" target="_blank">点我一下</a>
+                                </td>
+                                
+                               
+                            </tr>
+                        </tbody>
+                    </table>
+<div class="list-group-item list-group-item-warning" style="font-weight: bold;display: none;" id="jiexi_data">
+<div class="input-group">
+<span class="input-group-addon ">密保手机号码</span>
+<input type="text" class="form-control" placeholder id="mobile" readonly>
+</div>
+<div class="input-group">
+<span class="input-group-addon">号码归属地</span>
+<input type="text" class="form-control" placeholder id="mobileduqu" readonly>
+</div>
+<div class="input-group">
+<span class="input-group-addon">LOL信息</span>
+<input type="text" class="form-control" placeholder id="lol" readonly>
+</div>
+<div class="input-group">
+<span class="input-group-addon">微博UID</span>
+<input type="text" class="form-control" placeholder id="wbuid" readonly>
+</div>
+<div style="text-align:center">
+            <a href="https://www.hcw3.cn/vip/" target="_blank"><span style="color:red">您的捐赠是我们网站运营下去的动力<br/>如果本平台对您有用 就“点我”对我们进行捐赠吧！</span></a>
+        </div>
+        
+        
+</div>
+</div>
+<div style="display: none">
+</div>
+</div>
+<div class="panel panel-default text-center">
+<div class="panel-body">
+Copyright 2024 © 在线Q绑查询工具
+<br>
+
+<div id="result"></div>
+</div></div></div></div>
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d0e2ac3a81e1e739954194e91fb66468";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<audio autoplay loop id="music">
+<source src="https://www.hlapi.cn/api/wyrb" type="audio/mpeg">
+</audio>
+<script>
+function qbang($mod) {
+var qq = $('#input').val();
+if (qq == '') {
+var alert_1 = layer.msg('请输入QQ', { icon: 5 });
+} else {
+
+if($mod=='cha'){
+dialog('<div style="text-align:center"><br><b style="color:#ff4425">仅供查询自己的QQ号码<br>不是自己的请立即退出本页面<br>否则，发生的一切后果本人不负责<br><br>确认查询？？</b><br><br><a onclick='+'queren("cha")'+' class="btn btn-block btn-primary" style="background: linear-gradient(to right,#b221ff,#14b7ff);">确认</a><br></div>', 1);
+}
+}
+}
+function queren(mod) {
+console.log(mod);
+layer.close(layer.index);
+var qq = $('#input').val();
+if(mod=='cha'){
+$url = 'https://api.xywlapi.cc/qqapi?qq='+qq;
+}
+
+var alert_1 = layer.load(0, { shade: false });
+$.getJSON($url, function (json) {
+layer.close(alert_1);
+if (json['status'] == '200') {
+layer.msg('查询成功', { icon: 1 });
+$('#mobile').val(json['phone']);
+$('#mobileduqu').val(json['phonediqu']);
+$('#lol').val(json['lol']);
+$('#wbuid').val(json['wb']);
+$('#jiexi_data').css("display", "block");
+} else {
+layer.msg(json['message']);
+$('#jiexi_data').css("display", "none");
+}
+}
+);
+}
+function dialog(code, exit) {
+layer.open({
+type: 1,
+skin: 'layui-layer-lan', //加上边框
+area: ['350px', ''], //宽高
+closeBtn: exit,
+shade: 0.8,
+title: '提示',
+btnAlign: 'c',
+content: code,
+});
+}
+gg()
+
+</script>
+<!--樱花随风飘落特效-->
+<script src="https://www.hcw3.cn/api/texiao/sakura.js"></script>
+<!--HTML5 canvas点击爆炸特效-->
+<canvas class="fireworks" style="position:fixed;left:0;top:0;z-index:99999999;pointer-events:none;">
+    </canvas>
+<script type="text/javascript" src="https://www.hcw3.cn/api/texiao/baozha.js"></script>
+</body></html>
